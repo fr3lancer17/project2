a18e1230690c7b65d8aa8e8c354c32b92ca7e00b
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2024-01-17 13:48:02 +0000

    added basic navigation and routing for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,15 @@
 import React, {useEffect, useState} from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './components/Home.jsx';
+import Header from './components/Header.jsx';
+import About from './components/About.jsx';
+import Services from './components/Services.jsx';
+import Gallery from './components/Gallery.jsx';
+import Quote from './components/Quote.jsx';
+import Contact from './components/Contact.jsx';
+
+
 
 function App() {
   const [backendData, setBackendData] = useState([{}])
@@ -12,10 +23,22 @@ function App() {
     }
   )
 }, [])
-  return (
-    <div>
 
-    </div>
+  return (
+    <Router>
+      <Navbar />
+      {/* Wrap Route elements in a Routes component */}
+      <Routes>
+        {/* Define routes using the Route component to render different page components at different paths */}
+        {/* Define a default route that will render the Home component */}
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="services" element={<Services />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="quote" element={<Quote />} />
+        <Route path="contact/*" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import {Link} from react-router-dom;
+import {Link} from 'react-router-dom';
 
 
 function Navbar() {
   return (
-    <div>
-      <Link to={'/'}></Link>
-    </div>
+      
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link
+          to="/"
+          end
+        >
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="about"
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="services"
+        >
+          Services
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="gallery"
+          end
+        >
+          Gallery
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="quote"
+        >
+          Quote
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="contact"
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
   );
 }
 
