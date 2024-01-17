d2b4674752a651a021724a533c81b0322aa5c52f
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2024-01-17 10:51:00 +0000

    added dotenv package to devDependencies

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express()
+const dotenv = require('dotenv');
+
+// loads info from .env file
+dotenv.config();
 
 app.get("/api", (reg, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"] })
