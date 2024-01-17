f9e52bf6e8262d5384ae275d0381d2690d540725
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2024-01-17 13:31:03 +0000

    added components to app for basic nav routing

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from react-router-dom;
+
+
+function Navbar() {
+  return (
+    <div>
+      <Link to={'/'}></Link>
+    </div>
+  );
+}
+
+export default Navbar;
