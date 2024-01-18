897617a072b8a964fcfe8084c0797017f99ad82c
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2024-01-18 17:14:06 +0000

    added basic form details for quote component

--- client/src/components/Quote.jsx
+++ client/src/components/Quote.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
 function Quote() {
+
   return (
-    <div>
-      <h1>Quote Page</h1>
-      
+
+    <div className='container'>
+      <div className="row">
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="first_name" type="text" className="validate"/>
+            <label for="first_name">First Name</label>
+          </div>
+          <div className="input-field col s6">
+            <input id="last_name" type="text" className="validate"/>
+            <label for="last_name">Last Name</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="email" type="email" className="validate"/>
+            <label for="email">Email</label>
+          </div>
+          <div className="input-field col s6">
+            {/* <i className="material-icons prefix">phone</i> */}
+            <input id="icon_telephone" type="tel" className="validate"/>
+            <label for="icon_telephone">Telephone</label>
+          </div>
+        </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <textarea id="textarea1" className="materialize-textarea"></textarea>
+                <label for="textarea1">Textarea</label>
+              </div>
+        </div>
+      </form>
     </div>
+    </div>
+
   );
 }
 
