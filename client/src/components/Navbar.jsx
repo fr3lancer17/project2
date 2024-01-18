6b8da3d52f52ba9e9ae746a4ef3bae6493246f72
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2024-01-18 20:11:51 +0000

    added navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,53 +4,67 @@ import {Link} from 'react-router-dom';
 
 function Navbar() {
   return (
-      
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
-          to="/"
-          end
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="about"
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="services"
-        >
-          Services
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="gallery"
-          end
-        >
-          Gallery
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="quote"
-        >
-          Quote
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="contact"
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+    <>  
+    <nav>
+    <div className="nav-wrapper">
+      <a href="#!" className="brand-logo">Logo</a>
+      <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+      <ul className="right hide-on-med-and-down">
+        <li className="nav-item">
+          <Link
+            to="/"
+            end
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="about"
+          >
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="services"
+          >
+            Services
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="gallery"
+            end
+          >
+            Gallery
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="quote"
+          >
+            Quote
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="contact"
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+      </div>
+    </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+      <li><a href="collapsible.html">Javascript</a></li>
+      <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </>
   );
 }
 
