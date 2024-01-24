bda790a205401d23b77963b3650e8251fb5b5423
Renata Kiraly <kirrena@gmail.com>
2024-01-24 23:46:41 +0000

    added more content to Home page and increased Name font size and changed Company

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,10 +10,10 @@ function Header() {
     <div className="nav-wrapper">
       {/* <a href="#" className="brand-logo center">Logo</a> */}
       <ul id="nav-mobile" className="left hide-on-med-and-down">
-        <li className='logo-details'>Steve Brouthtoen</li>
+        <li className='logo-details'><h6>Steve Brouthtoen</h6></li>
       </ul>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li className='logo-details'>Company</li>
+        <li className='logo-details'><h6>Art, Design, Decoration</h6></li>
       </ul>
     </div>
   </nav>
