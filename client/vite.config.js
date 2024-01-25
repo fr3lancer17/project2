b6a432a37248c831f5d2c94b7c87aa1486ecd39b
bloodfued12 <benjamin.smith.ep@gmail.com>
2024-01-25 21:32:20 +0000

    added external dependancies for build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactMaterialize from 'react-materialize'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-materialize', 'react-dom/client'],
+    },
+  },
 })
