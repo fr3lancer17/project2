a7e4777a47430781996302768711d69b53afaf82
bloodfued12 <benjamin.smith.ep@gmail.com>
2024-01-25 18:52:31 +0000

    final project

--- client/src/components/GalleryCarousel.jsx
+++ client/src/components/GalleryCarousel.jsx
@@ -1,24 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Carousel } from 'react-materialize';
-import imagesJson from '../img.json';
+// import imagesJson from '../img.json';
 
 const GalleryCarousel = () => {
-  const [images, setImages] = useState([]);
+//   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    const imagesArr = imagesJson; // assuming 'imagesJson' is an array in 'img.json'
-    const extractedImages = imagesArr.map((image) => Object.values(image)[0]);
-    setImages(extractedImages);
-  }, []);
+//   useEffect(() => {
+//     const imagesArr = imagesJson; // assuming 'imagesJson' is an array in 'img.json'
+//     const extractedImages = imagesArr.map((image) => Object.values(image)[0]);
+//     setImages(extractedImages);
+//   }, []);
 
   return (
-    <Carousel options={{ fullWidth: true }}>
-      {images.map((src, index) => (
-        <div key={index}>
-          <img src={src} alt="" />
-        </div>
-      ))}
-    </Carousel>
+    <Carousel
+    images={[
+        '../images/image1.jpg',
+        '../images/image2.jpg',
+        '../images/image3.jpg',
+        '../images/image4.jpg',
+        '../images/image5.jpg',
+        '../images/image6.jpg',
+        '../images/image7.jpg',
+        '../images/image8.jpg',
+        '../images/image9.jpg',
+        '../images/image10.jpg',
+        '../images/image11.jpg'
+    ]}
+    options={{
+      dist: -100,
+      duration: 200,
+      fullWidth: false,
+      indicators: false,
+      noWrap: false,
+      numVisible: 5,
+      onCycleTo: null,
+      padding: 0,
+      shift: 0
+    }}
+  />
   );
 };
 
